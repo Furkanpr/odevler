30da92fbe734ecd87bedc12818f6fa86c4101b73
Furkanpr <fuurkandemiir@gmail.com>
2021-11-01 23:40:50 +0300

    Create scanf.java

---
+++ src/odevler/scanf.java
@@ -0,0 +1,30 @@
+package odevler;
+
+import java.util.Locale;
+import java.util.Scanner; // ctrl+shift+o 
+
+public class scanf {
+
+	public static void main(String[] args) {
+
+		
+	Scanner deger = new Scanner (System.in);
+	deger.useLocale(Locale.US) ;
+	
+	System.out.println("bir sayi giriniz: ");
+	
+	double a =  deger.nextDouble();
+		
+		
+	System.out.println("a nin deðerý " +a+ " dir");
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
